981532372fdf1e455eeb43dca5a4680eef7374df
JaviBravoPerucho <125208949+JaviBravoPerucho@users.noreply.github.com>
2026-04-24 10:36:02 +0200

    Lectura de nota

---
+++ Assets/NoteTrigger.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NoteTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pressERead;
+
+    [SerializeField]
+    GameObject pressESave;
+
+    [SerializeField]
+    GameObject noteUI;
+
+    [SerializeField]
+    GameObject noteObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pressERead.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pressERead.SetActive(false);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pressERead.activeSelf && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            noteObject.SetActive(false);
+            noteUI.SetActive(true);
+            pressERead.SetActive(false);
+            pressESave.SetActive(true);
+        }else if(pressESave.activeSelf && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            noteUI.SetActive(false);
+            pressESave.SetActive(false);
+        }
+    }
+}
