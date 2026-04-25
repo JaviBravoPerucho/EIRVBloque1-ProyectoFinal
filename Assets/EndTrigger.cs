c9996741cf1b1963bb13c5c99c9d410671d35035
JaviBravoPerucho <125208949+JaviBravoPerucho@users.noreply.github.com>
2026-04-25 20:40:59 +0200

    Grito final

---
+++ Assets/EndTrigger.cs
@@ -0,0 +1,31 @@
+using StarterAssets;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+
+    public AudioSource source;
+    public AudioClip sound;
+
+    public FirstPersonController controller;
+
+    public GameObject endTitle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        source.PlayOneShot(sound);
+        GetComponent<BoxCollider>().enabled = false;
+        controller.enabled = false;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
