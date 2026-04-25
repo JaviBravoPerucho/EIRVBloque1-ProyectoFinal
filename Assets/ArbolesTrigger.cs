55ab50b5e3fe9fddeefa947e29f27feb97bfa325
JaviBravoPerucho <125208949+JaviBravoPerucho@users.noreply.github.com>
2026-04-25 19:50:11 +0200

    Ajuste de sangre

---
+++ Assets/ArbolesTrigger.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ArbolesTrigger : MonoBehaviour
+{
+
+    float elapsedTime = 0;
+    float growingTime = 30.0f;
+    float growScale = 10.0f;
+    public GameObject trees;
+    bool startGrow = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        startGrow = true;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startGrow)
+        {
+            elapsedTime += Time.deltaTime;
+            Debug.Log(elapsedTime);
+            if (elapsedTime < growingTime)
+            {
+                Vector3 grow;
+                grow.x = 1 + (elapsedTime / growingTime) * growScale;
+                grow.y = 1 + (elapsedTime / growingTime) * growScale;
+                grow.z = 1 + (elapsedTime / growingTime) * growScale;
+                foreach (Transform arbol in trees.transform)
+                {
+                    arbol.localScale = grow;
+                }
+            }
+            else startGrow = false;
+        }
+    }
+}
